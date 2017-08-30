2326ae60780c41cdddcb8fe59b40a70a5dc79b32
evanxia <evanxia1018@gmail.com>
2017-08-29 23:49:00 -0700

    Initial commit

---
+++ src/main/java/life/sheea/PersonalWebsiteApplication.java
@@ -0,0 +1,14 @@
+package life.sheea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by evanxia on 8/24/17.
+ */
+@SpringBootApplication
+public class PersonalWebsiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalWebsiteApplication.class, args);
+    }
+}
---
+++ src/main/java/life/sheea/controller/GreetingController.java
@@ -0,0 +1,12 @@
+package life.sheea.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by evanxia on 8/24/17.
+ */
+
+@Controller
+public class GreetingController {
+
+}
